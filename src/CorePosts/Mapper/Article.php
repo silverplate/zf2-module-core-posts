59559d7adb83805c4ce6a41e96f14ae274f80248
Kirill Brusenskiy <kirill@brusenskiy.ru>
2014-07-08 10:37:47 +0400

    Метод получения авторов для списка записей.

--- src/CorePosts/Mapper/Article.php
+++ src/CorePosts/Mapper/Article.php
@@ -4,4 +4,42 @@ namespace CorePosts\Mapper;
 
 class Article extends AbstractPost
 {
+    /**
+     * @param \CorePosts\Entity\Article[] $articles
+     * @return \CorePosts\Entity\Article[]
+     */
+    public function fetchAuthors($articles)
+    {
+        $authorIds = array();
+        foreach ($articles as $article) {
+            if ($article->getAuthorId()) {
+                $authorIds[] = $article->getAuthorId();
+            }
+        }
+
+        if (count($authorIds) > 0) {
+            $l = $this->getAuthorMapper()
+                      ->fetchAll(array('person_id' => array_unique($authorIds)))
+                      ->asArray();
+
+            foreach ($articles as $article) {
+                if (
+                    $article->getAuthorId() &&
+                    array_key_exists($article->getAuthorId(), $l)
+                ) {
+                    $article->setAuthor($l[$article->getAuthorId()]);
+                }
+            }
+        }
+
+        return $articles;
+    }
+
+    /**
+     * @return \CorePosts\Mapper\Author
+     */
+    public function getAuthorMapper()
+    {
+        return $this->srv('\CorePosts\Mapper\Author');
+    }
 }
